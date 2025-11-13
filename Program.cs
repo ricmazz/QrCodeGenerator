425e2be43623aad192b564bda7b1a3c724eb0f31
Ricardo Mazzarioli <ricardomazzarioli@outlook.com>
2025-11-13 00:32:21 -0300

    Add support for customizable QR code colors via command-line arguments.

--- Program.cs
+++ Program.cs
@@ -7,18 +7,21 @@ class Program
     {
         string qrText = string.Empty; // The text or URL for the QR code
         string fileName = string.Empty;
+        string colorQrCode = string.Empty;
+        
         var previousColor = Console.ForegroundColor;
         
-        if (args != null && args.Length == 2)
+        if (args != null && args.Length == 3)
         {
             qrText = args[0];
             fileName = args[1];
+            colorQrCode = args[2];
         }
         else
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Please provide text and file name.");
-            Console.WriteLine("Usage: dotnet run <text> <file_name>");
+            Console.WriteLine("Please provide text, file name and color.");
+            Console.WriteLine("Usage: dotnet run <text> <file_name> <color>");
             Console.ForegroundColor = previousColor;
             return;
         }
@@ -32,7 +35,7 @@ class Program
             // Generate SVG QR code
             SvgQRCode qrCode = new SvgQRCode(qrCodeData);
             string svgQRCode =
-                qrCode.GetGraphic(20, "#D41367", "#FFFFFF00"); // Set background to transparent with "#FFFFFF00"
+                qrCode.GetGraphic(20, colorQrCode, "#FFFFFF00"); // Set background to transparent with "#FFFFFF00"
 
             // Output the SVG
             Directory.CreateDirectory("qrCodes");
